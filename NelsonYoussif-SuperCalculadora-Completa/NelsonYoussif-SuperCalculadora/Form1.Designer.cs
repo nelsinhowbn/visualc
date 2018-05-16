abc788f530cd746a59756d3e905dd2526931e106
nelsinhowbn <nelsonhadi95@gmail.com>
2018-05-15 21:51:10 -0300

    Calculadora  Completa

---
+++ NelsonYoussif-SuperCalculadora-Completa/NelsonYoussif-SuperCalculadora/Form1.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace NelsonYoussif_SuperCalculadora
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.painel = new System.Windows.Forms.TextBox();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btnmais = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btnmenos = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btnmult = new System.Windows.Forms.Button();
+            this.btn0 = new System.Windows.Forms.Button();
+            this.btndiv = new System.Windows.Forms.Button();
+            this.btnigual = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // painel
+            // 
+            this.painel.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.painel.Location = new System.Drawing.Point(31, 12);
+            this.painel.Name = "painel";
+            this.painel.Size = new System.Drawing.Size(267, 80);
+            this.painel.TabIndex = 0;
+            this.painel.Text = "0";
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(50, 121);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(41, 30);
+            this.btn1.TabIndex = 1;
+            this.btn1.Text = "1";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(112, 121);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(44, 30);
+            this.btn2.TabIndex = 2;
+            this.btn2.Text = "2";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(184, 120);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(44, 30);
+            this.btn3.TabIndex = 3;
+            this.btn3.Text = "3";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // btnmais
+            // 
+            this.btnmais.Location = new System.Drawing.Point(252, 121);
+            this.btnmais.Name = "btnmais";
+            this.btnmais.Size = new System.Drawing.Size(46, 30);
+            this.btnmais.TabIndex = 4;
+            this.btnmais.Text = "+";
+            this.btnmais.UseVisualStyleBackColor = true;
+            this.btnmais.Click += new System.EventHandler(this.btnmais_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.Location = new System.Drawing.Point(50, 186);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(41, 30);
+            this.btn4.TabIndex = 5;
+            this.btn4.Text = "4";
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.Location = new System.Drawing.Point(112, 186);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(44, 30);
+            this.btn5.TabIndex = 6;
+            this.btn5.Text = "5";
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.Location = new System.Drawing.Point(184, 186);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(44, 30);
+            this.btn6.TabIndex = 7;
+            this.btn6.Text = "6";
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // btnmenos
+            // 
+            this.btnmenos.Location = new System.Drawing.Point(252, 186);
+            this.btnmenos.Name = "btnmenos";
+            this.btnmenos.Size = new System.Drawing.Size(46, 30);
+            this.btnmenos.TabIndex = 8;
+            this.btnmenos.Text = "-";
+            this.btnmenos.UseVisualStyleBackColor = true;
+            this.btnmenos.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.Location = new System.Drawing.Point(50, 250);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(41, 28);
+            this.btn7.TabIndex = 9;
+            this.btn7.Text = "7";
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.btn7_Click);
+            // 
+            // btn9
+            // 
+            this.btn9.Location = new System.Drawing.Point(184, 250);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(41, 30);
+            this.btn9.TabIndex = 10;
+            this.btn9.Text = "9";
+            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Click += new System.EventHandler(this.btn9_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.Location = new System.Drawing.Point(112, 250);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(41, 30);
+            this.btn8.TabIndex = 11;
+            this.btn8.Text = "8";
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // btnmult
+            // 
+            this.btnmult.Location = new System.Drawing.Point(252, 250);
+            this.btnmult.Name = "btnmult";
+            this.btnmult.Size = new System.Drawing.Size(46, 30);
+            this.btnmult.TabIndex = 12;
+            this.btnmult.Text = "x";
+            this.btnmult.UseVisualStyleBackColor = true;
+            this.btnmult.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // btn0
+            // 
+            this.btn0.Location = new System.Drawing.Point(144, 298);
+            this.btn0.Name = "btn0";
+            this.btn0.Size = new System.Drawing.Size(41, 30);
+            this.btn0.TabIndex = 13;
+            this.btn0.Text = "0";
+            this.btn0.UseVisualStyleBackColor = true;
+            this.btn0.Click += new System.EventHandler(this.btn0_Click);
+            // 
+            // btndiv
+            // 
+            this.btndiv.Location = new System.Drawing.Point(252, 304);
+            this.btndiv.Name = "btndiv";
+            this.btndiv.Size = new System.Drawing.Size(46, 30);
+            this.btndiv.TabIndex = 14;
+            this.btndiv.Text = "/";
+            this.btndiv.UseVisualStyleBackColor = true;
+            this.btndiv.Click += new System.EventHandler(this.btndiv_Click);
+            // 
+            // btnigual
+            // 
+            this.btnigual.Location = new System.Drawing.Point(252, 364);
+            this.btnigual.Name = "btnigual";
+            this.btnigual.Size = new System.Drawing.Size(46, 30);
+            this.btnigual.TabIndex = 15;
+            this.btnigual.Text = "=";
+            this.btnigual.UseVisualStyleBackColor = true;
+            this.btnigual.Click += new System.EventHandler(this.btnigual_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(50, 302);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "Limpar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 95);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 17;
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(335, 399);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnigual);
+            this.Controls.Add(this.btndiv);
+            this.Controls.Add(this.btn0);
+            this.Controls.Add(this.btnmult);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btnmenos);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btnmais);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.painel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox painel;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btnmais;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btnmenos;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btnmult;
+        private System.Windows.Forms.Button btn0;
+        private System.Windows.Forms.Button btndiv;
+        private System.Windows.Forms.Button btnigual;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ NelsonYoussif-SuperCalculadora-Completa/NelsonYoussif-SuperCalculadora/Form1.cs
@@ -0,0 +1,523 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace NelsonYoussif_SuperCalculadora
+{
+    public partial class Form1 : Form
+    {
+
+        double n1, r,n2;
+        
+        int op,v=0; 
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            label1.Text = label1.Text + "6";
+            if (painel.Text == "0")
+                painel.Text = "6";
+
+            else
+            {
+                painel.Text = (painel.Text) + "6";
+            }
+
+            if (v == 0) // Se a operação for igual a 0, e o primeiro numero for diferente do 0 ele ira acrescentar esse numero *10 e mais o numero digitado
+            {
+                
+                if (n1 != 0) {
+                   n1=n1 * 10;
+                }
+                n1 = n1 + 6;
+            }
+            else {
+                if (n2 != 0)
+                {
+                    n2 = n2 * 10;
+                }
+                n2 = n2 + 6;
+            }
+        } 
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            label1.Text = label1.Text + "8";
+            if (painel.Text == "0")
+                painel.Text = "8";
+
+            else
+            {
+                painel.Text = (painel.Text) + "8";
+            }
+            if (v == 0)
+            {
+
+                if (n1 != 0)
+                {
+                    n1 = n1 * 10;
+                }
+                n1 = n1 + 8;
+            }
+            else
+            {
+                if (n2 != 0)
+                {
+                    n2 = n2 * 10;
+                }
+                n2 = n2 + 8;
+            }
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            label1.Text = label1.Text + "x";
+
+            if (v >= 1)
+            {
+                switch (op)
+                {
+                    case 1:
+                        r = n1 * n2;
+                        painel.Text = r.ToString();
+                        break;
+                    case 2:
+                        r = n1 + n2;
+                        painel.Text = r.ToString();
+                        break;
+                    case 3:
+                        r = n1 - n2;
+                        painel.Text = r.ToString();
+                        break;
+                    case 4:
+                        r = n1 / n2;
+                        painel.Text = r.ToString();
+                        break;
+                }
+                n1 = r;
+                n2 = 0;
+            }
+            n1 = float.Parse(painel.Text);
+            painel.Clear();
+            painel.Focus();
+            op = 1;
+            v++; // adiciona uma variavel V
+           
+
+           
+           
+        }
+
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            label1.Text = label1.Text + "-";
+            if (v >= 1)
+            {
+                switch (op)
+                {
+                    case 1:
+                        r = n1 * n2;
+                        painel.Text = r.ToString();
+                        break;
+                    case 2:
+                        r = n1 + n2;
+                        painel.Text = r.ToString();
+                        break;
+                    case 3:
+                        r = n1 - n2;
+                        painel.Text = r.ToString();
+                        break;
+                    case 4:
+                        r = n1 / n2;
+                        painel.Text = r.ToString();
+                        break;
+                }
+                n1 = r;
+                n2 = 0;
+            }
+            n1 = float.Parse(painel.Text);
+            painel.Clear();
+            painel.Focus();
+            op = 3;
+            v++;
+
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            label1.Text = label1.Text + "1";
+            if (painel.Text == "0")
+                painel.Text = "1";
+
+            else
+            {
+                painel.Text = (painel.Text) + "1";
+            }
+            if (v == 0)
+            {
+
+                if (n1 != 0)
+                {
+                    n1 = n1 * 10;
+                }
+                n1 = n1 + 1;
+            }
+            else
+            {
+                if (n2 != 0)
+                {
+                    n2 = n2 * 10;
+                }
+                n2 = n2 + 1;
+            }
+
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            label1.Text = label1.Text + "2";
+            if (painel.Text == "0")
+                painel.Text = "2";
+
+            else
+            {
+                painel.Text = (painel.Text) + "2";
+            }
+            if (v == 0)
+            {
+
+                if (n1 != 0)
+                {
+                    n1 = n1 * 10;
+                }
+                n1 = n1 + 2;
+            }
+            else
+            {
+                if (n2 != 0)
+                {
+                    n2 = n2 * 10;
+                }
+                n2 = n2 + 2;
+            }
+
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            label1.Text = label1.Text + "3";
+            if (painel.Text == "0")
+                painel.Text = "3";
+
+            else
+            {
+                painel.Text = (painel.Text) + "3";
+            }
+            if (v == 0)
+            {
+
+                if (n1 != 0)
+                {
+                    n1 = n1 * 10;
+                }
+                n1 = n1 + 3;
+            }
+            else
+            {
+                if (n2 != 0)
+                {
+                    n2 = n2 * 10;
+                }
+                n2 = n2 + 3;
+            }
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            label1.Text = label1.Text + "4";
+            if (painel.Text == "0")
+                painel.Text = "4";
+
+            else
+            {
+                painel.Text = (painel.Text) + "4";
+            }
+            if (v == 0)
+            {
+
+                if (n1 != 0)
+                {
+                    n1 = n1 * 10;
+                }
+                n1 = n1 + 4;
+            }
+            else
+            {
+                if (n2 != 0)
+                {
+                    n2 = n2 * 10;
+                }
+                n2 = n2 + 4;
+            }
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            label1.Text = label1.Text + "5";
+            if (painel.Text == "0")
+                painel.Text = "5";
+
+            else
+            {
+                painel.Text = (painel.Text) + "5";
+            }
+            if (v == 0) 
+            {
+
+                if (n1 != 0)
+                {
+                    n1 = n1 * 10;
+                }
+                n1 = n1 + 5;
+            }
+                
+            else
+            {
+                if (n2 != 0)
+                {
+                    n2 = n2 * 10;
+                }
+                n2 = n2 + 5;
+            }
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            label1.Text = label1.Text + "7";
+            if (painel.Text == "0")
+                painel.Text = "7";
+
+            else
+            {
+                painel.Text = (painel.Text) + "7";
+            }
+            if (v == 0)
+            {
+
+                if (n1 != 0)
+                {
+                    n1 = n1 * 10;
+                }
+                n1 = n1 + 7;
+            }
+            else
+            {
+                if (n2 != 0)
+                {
+                    n2 = n2 * 10;
+                }
+                n2 = n2 + 7;
+            }
+        }
+
+    
+
+        private void btn0_Click(object sender, EventArgs e)
+        {
+            label1.Text = label1.Text + "0";
+            
+            if (painel.Text == "")
+                painel.Text = "0";
+
+            else
+            {
+                painel.Text = (painel.Text) + "0";
+            }
+            if (v == 0)
+            {
+
+                if (n1 != 0)
+                {
+                    n1 = n1 * 10;
+                }
+                n1 = n1 + 0;
+            }
+            else
+            {
+                if (n2 != 0)
+                {
+                    n2 = n2 * 10;
+                }
+                n2 = n2 + 0;
+            }
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            label1.Text = label1.Text + "9";
+            if (painel.Text == "0")
+                painel.Text = "9";
+
+            else
+            {
+                painel.Text = (painel.Text) + "9";
+            }
+            if (v == 0)
+            {
+
+                if (n1 != 0)
+                {
+                    n1 = n1 * 10;
+                }
+                n1 = n1 + 9;
+            }
+            else
+            {
+                if (n2 != 0)
+                {
+                    n2 = n2 * 10;
+                }
+                n2 = n2 + 9;
+            }
+        }
+
+        private void btndiv_Click(object sender, EventArgs e)
+        {
+            label1.Text = label1.Text + "/";
+
+            if (v >= 1)
+            {
+                switch (op)
+                {
+                    case 1:
+                        r = n1 * n2;
+                        painel.Text = r.ToString();
+                        break;
+                    case 2:
+                        r = n1 + n2;
+                        painel.Text = r.ToString();
+                        break;
+                    case 3:
+                        r = n1 - n2;
+                        painel.Text = r.ToString();
+                        break;
+                    case 4:
+                        r = n1 / n2;
+                        painel.Text = r.ToString();
+                        break;
+                }
+                n1 = r;
+                n2 = 0;
+            }
+            n1 = float.Parse(painel.Text);
+            painel.Clear();
+            painel.Focus();
+            op = 4;
+            v++;
+
+        }
+
+        private void btnmais_Click(object sender, EventArgs e)
+        {
+            label1.Text = label1.Text + "+";
+            if (v >= 1)
+            {
+                switch (op)
+                {
+                    case 1:
+                        r = n1 * n2;
+                        painel.Text = r.ToString();
+                        break;
+                    case 2:
+                        r = n1 + n2;
+                        painel.Text = r.ToString();
+                        break;
+                    case 3:
+                        r = n1 - n2;
+                        painel.Text = r.ToString();
+                        break;
+                    case 4:
+                        r = n1 / n2;
+                        painel.Text = r.ToString();
+                        break;
+                }
+                n1 = r;
+                n2 = 0;
+                painel.Text = "" + r;
+            }
+            n1 = float.Parse(painel.Text);
+            painel.Clear();
+            painel.Focus();
+            op = 2;
+            v++;
+
+            }
+
+
+
+        private void btnigual_Click(object sender, EventArgs e)
+        {
+            label1.Text = label1.Text + "=";
+
+
+           // painel.Text += "=";
+
+            switch (op)
+            {  
+                case 1:
+                    r = n1 * n2;
+                    painel.Text = r.ToString();   // operação de multiplicação
+                    break;
+                case 2:
+                    r = n1 + n2;
+                    painel.Text = r.ToString(); // opração de adição
+                    break;
+                case 3:
+                    r = n1 - n2;
+                    painel.Text = r.ToString(); 
+                    break;
+                case 4:
+                    r = n1 / n2;
+                    painel.Text = r.ToString(); 
+                    break;
+            }
+
+            n1 = r;
+            n2 = 0;
+            label1.Text = label1.Text + r;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            r = 0;
+            n1 = 0;
+            n2 = 0;
+            v = 0;
+            painel.Text = "";
+            label1.Text = "";
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+           
+        }
+        
+
+    
+    }
+}
